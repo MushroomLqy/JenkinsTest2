0f49e43a7f9991dcb96e2e2304e292cacc52db34
mac <mac@macdeMacBook-Pro.local>
2017-10-11 14:08:49 +0800

    上传项目代码

---
+++ app/src/main/java/com/andli/jenkins/MainActivity.java
@@ -0,0 +1,19 @@
+package com.andli.jenkins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *author andli
+ *create at 16/6/27 下午9:01
+ *
+**/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
